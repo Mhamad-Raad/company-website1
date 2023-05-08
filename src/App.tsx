2d3c4a458e51725893d9dd9a06193ca556589868
joker69216 <hamaraad883@gmail.com>
2023-05-08 22:19:11 +0300

    add a scruture for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Navbar from './components/Navbar/Navbar';
+
 import './App.scss';
 
 function App() {
   return (
     <div className='app'>
+      <Navbar />
       <h1 className='text-3xl font-bold underline'>hello world</h1>
     </div>
   );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  return <nav></nav>;
+};
+
+export default Navbar;
