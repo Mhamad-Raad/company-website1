e5cecd17ea329e8abc2aa22b5e2ca8701b104f13
joker69216 <hamaraad883@gmail.com>
2023-05-09 17:57:14 +0300

    add Home Screen Structure

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Hero: FC<{}> = () => {
+  return (
+    <section className='w-full h-screen bg-zinc-200'>
+    Hero
+    </section>
+  );
+};
+
+export default Hero;
